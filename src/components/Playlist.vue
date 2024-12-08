e712340715220ec1622a36e86a3a8c6786ffcddc
Hemalatha467 <83014822+Hemalatha467@users.noreply.github.com>
2024-12-08 14:13:12 -0500

    Add files via upload
    playlist and single song components added

---
+++ src/components/Playlist.vue
@@ -0,0 +1,28 @@
+<template>   
+        <div v-for="song in songs.slice(1)" :key="song.songId" class="songCard" >
+            <SingleSong :song=song />
+        </div>
+
+</template>
+<script>
+import SingleSong from './SingleSong.vue';
+
+export default {
+    name: 'MusicPlaylist',
+    props:{
+        songs: Array
+    },
+    components:{
+        SingleSong
+    }
+    
+}
+</script>
+<style>
+    .songCard {
+        width:25%;
+        padding:16px;
+        border: 2px solid #cde1e7;
+
+    }
+</style>
---
+++ src/components/SingleSong.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+
+      <img :src="song.image" alt="cover" width="100%" height="400px" />
+      <h2> {{song.title}} </h2>
+      <h4> Singer: <span>{{song.singer }}</span></h4>
+      <h4> Album: <span>{{song.albumName}}</span> </h4>
+      <h4> Song Duration: <span>{{ song.songDuration }}</span></h4>
+      <h4> Released Date: <span>{{song.releasedDate}}</span></h4>
+    </div>
+</template>
+
+<script>
+
+export default{
+    name: 'SingleBook',
+    props:{
+        song: Array
+    }
+}
+</script>
+
+<style>
+        span{
+            font-weight: 590;
+        }
+</style>
